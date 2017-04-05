ed048fe39572ca0310ba0df8e3751e8612b6869d
Ivan Masmitjà <imasmitja@gmail.com>
2017-04-05 12:44:53 +0200

    Add EmptyKey

---
+++ WpfKb/LogicalKeys/EmptyKey.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfKb.LogicalKeys
+{
+    public class EmptyKey : LogicalKeyBase
+    {
+
+        public EmptyKey(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public EmptyKey()
+        {
+        }
+    }
+}
